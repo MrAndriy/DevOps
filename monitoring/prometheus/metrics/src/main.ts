28ecb9c078cf9f9be8da6d0494549188f1a73fc9
Andriy Mriy <a.mriy.lionwood@gmail.com>
2022-12-02 10:23:11 +0200

    feat:monitoring

---
+++ monitoring/prometheus/metrics/src/main.ts
@@ -0,0 +1,12 @@
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000, '0.0.0.0')
+  //log the url with nestJS logger
+  Logger.debug(`Application is running on: ${await app.getUrl()}`)
+}
+bootstrap()
